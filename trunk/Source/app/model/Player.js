3d1abde5a5b07f003b4d3c7c8cd26173d7d0d8c2
bd <bdletterbox@gmail.com>
2012-12-23 13:35:23 +0400

    Player class added

---
+++ trunk/Source/app/model/Player.js
@@ -0,0 +1,22 @@
+/*
+ * Класс данных об игроке
+ *
+ * Данные об игроке:
+ * Имя,
+ * Очки
+ */
+
+Ext.define('CrazyHat.model.Player', {
+    extend: 'Ext.data.Model',
+
+    config: {
+        fields: [
+            // Имя
+            {name: 'name',  type: 'string', defaultValue: ''},
+            // Очки
+            {name: 'score',   type: 'int', defaultValue: 0}
+        ]
+    }
+
+});
+
